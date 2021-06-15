936c405165ff6897b5cfff5e8c17f3fd8e7fdb05
edo0 <16632292+edo0@users.noreply.github.com>
2021-06-15 16:31:44 +0200

    Initial commit
    Credits to https://github.com/black7375/Firefox-UI-Fix for the SVG icon and part of the config files

---
+++ user.js
@@ -0,0 +1,19 @@
+// ** Theme Related Options ****************************************************
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS Blur Filter - 88 Above
+user_pref("layout.css.backdrop-filter.enabled", true);
+
+// Restore Compact Mode - 89 Above
+user_pref("browser.compactmode.show", true);
+
+// about:home Search Bar
+// user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+
+// ** Useful Options ***********************************************************
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
